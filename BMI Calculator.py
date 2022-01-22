7e73603c9172f9abc2f19891d8c40caebdd2c492
Walden1124 <61943392+Walden1124@users.noreply.github.com>
2022-01-22 17:04:24 -0500

    BMI Calculator

---
+++ BMI Calculator.py
@@ -0,0 +1,8 @@
+print("This is a Body Mass Index Calculator \n" "To calculate your BMI we'll need some information from you first")
+height = input("\nEnter your height in inches: ")
+weight = input("Enter your weight in lbs: ")
+new_height = float(height)
+new_weight = int(weight)
+result = int((new_weight / new_height ** 2) * 703)
+print("\nThis is your BMI:" + str(result))
+print("\nThe CDC states that a healthy BMI ranges from 18.5 - 24.9")
